e4ec6d7e92483a48f763a22d3f6f7c19df5e70b9
Michael Martin-Smucker <mlmartin13@gmail.com>
2016-01-04 22:24:05 -0700

    Start to add thx http for api requests

--- client/app/Reducers.hx
+++ client/app/Reducers.hx
@@ -1,9 +1,20 @@
 package app;
 
 import lies.Reduced;
+import thx.http.Request;
+import thx.http.RequestInfo;
 
 class Reducers {
-  public static function mtgApp(state : State, action : Action) {
+  public static function mtgApp(state : State, action : Action) return switch action {
+    case RequestInitialData: getCards(state);
+    case _: Reduced.fromState(state);
+  }
+
+  static function getCards(state) {
+    var info = new RequestInfo(Get, thx.Url.fromString("/api/cards"));
+
+    // TODO: async state magic
+    Request.make(info);
     return Reduced.fromState(state);
   }
 }
